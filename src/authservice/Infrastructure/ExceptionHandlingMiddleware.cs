6daaa1998c58cc1b92dcbc87d9355a7af3587d3d
Mariusz Sokołowski <mariuszsokolowski95@gmail.com>
2024-10-14 21:58:37 +0200

    Add middleware for exception handling

---
+++ src/authservice/Infrastructure/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,40 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace authservice.Infrastructures
+{
+
+    public class ExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public ExceptionHandlingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private Task HandleExceptionAsync(HttpContext context, Exception ex)
+        {
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            context.Response.ContentType = "application/json";
+
+            var response = new
+            {
+                StatusCode = context.Response.StatusCode,
+                Message = ex.Message 
+            };
+
+            return context.Response.WriteAsync(JsonSerializer.Serialize(response));
+        }
+    }
+}
