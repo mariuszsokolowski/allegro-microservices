70eb89801edece9f433448a012389554d6a52239
Mariusz Sokołowski <mariuszsokolowski95@gmail.com>
2024-10-14 15:06:49 +0200

    Update settingsService

---
+++ src/authservice/Extensions/ConfigurationExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace authservice.Extensions
+{
+    public static class ConfigurationExtensions
+    {
+        public static string GetValueOrDefault(this IConfiguration configuration, string key)
+        {
+            return configuration.GetValue<string>(key) ?? "";
+        }
+    }
+}
--- src/authservice/Services/SettingsService.cs
+++ src/authservice/Services/SettingsService.cs
@@ -1,6 +1,8 @@
 ﻿using authservice.Interfaces;
 using authservice.Models;
+using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json.Linq;
+using authservice.Extensions;
 
 namespace authservice.Services
 {
@@ -8,24 +10,33 @@ namespace authservice.Services
     {
         private readonly AllegroSettingsModel _allegroSetttings;
         private readonly IConfiguration _configuration;
+        private readonly string _appsettingsPath;
         public SettingsService(IConfiguration configuration)
         {
             _configuration = configuration;
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production";
+            if (environment == "Production")
+            {
+                _appsettingsPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "appsettings.json");
+            }
+            else
+            {
+                _appsettingsPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, $"appsettings.{environment}.json");
+            }
+
             _allegroSetttings = new AllegroSettingsModel()
             {
-                BaseUrl = _configuration["Allegro:BaseUrl"].ToString(),
-                ClientId = _configuration["Allegro:ClientId"].ToString(),
-                ClientSecret = _configuration["Allegro:ClientSecret"].ToString(),
-                DeviceCode = _configuration["Allegro:DeviceCode"].ToString(),
-                AccessTokenExpiredDate = !String.IsNullOrEmpty(_configuration["Allegro:AccessTokenExpiredDate"]) ? Convert.ToDateTime(_configuration["Allegro:AccessTokenExpiredDate"]) : null,
-                AccessToken = _configuration["Allegro:AccessToken"],
-                RefreshToken = _configuration["Allegro:RefreshToken"],
-                VerificationURIComplete = _configuration["Allegro:VerificationURIComplete"]
+                BaseUrl = _configuration.GetValueOrDefault("Allegro:BaseUrl"),
+                ClientId = _configuration.GetValueOrDefault("Allegro:ClientId"),
+                ClientSecret = _configuration.GetValueOrDefault("Allegro:ClientSecret"),
+                DeviceCode = _configuration.GetValueOrDefault("Allegro:DeviceCode"),
+                AccessTokenExpiredDate = !String.IsNullOrEmpty(_configuration.GetValueOrDefault("Allegro:AccessTokenExpiredDate")) ? Convert.ToDateTime(_configuration["Allegro:AccessTokenExpiredDate"]) : null,
+                AccessToken = _configuration.GetValueOrDefault("Allegro:AccessToken"),
+                RefreshToken = _configuration.GetValueOrDefault("Allegro:RefreshToken"),
+                VerificationURIComplete = _configuration.GetValueOrDefault("Allegro:VerificationURIComplete")
             };
         }
-
         public string GetAccessToken() => _allegroSetttings.AccessToken;
-
         public string GetBaseUrl() => _allegroSetttings.BaseUrl;
         public string GetClientId() => _allegroSetttings.ClientId;
         public string GetClientSecret() => _allegroSetttings.ClientSecret;
@@ -38,32 +49,57 @@ namespace authservice.Services
 
         public void SetAccessToken(string accessToken)
         {
-            _configuration["Allegro:AccessToken"] = accessToken;
+            Save("Allegro.AccessToken", accessToken);
             _allegroSetttings.AccessToken = accessToken;
         }
 
         public void SetAccessTokenExpiredDate(DateTime accessTokenExpiredDate)
         {
-            _configuration["Allegro:AccessTokenExpiredDate"] = accessTokenExpiredDate.ToString();
+            Save("Allegro.AccessTokenExpiredDate", accessTokenExpiredDate.ToString());
             _allegroSetttings.AccessTokenExpiredDate = accessTokenExpiredDate;
         }
 
         public void SetDeviceCode(string deviceCode)
         {
-            _configuration["Allegro:DeviceCode"] = deviceCode;
+            Save("Allegro.DeviceCode", deviceCode);
             _allegroSetttings.DeviceCode = deviceCode;
         }
 
         public void SetRefreshToken(string refreshToken)
         {
-            _configuration["Allegro:RefreshToken"] = refreshToken;
+            Save("Allegro.RefreshToken", refreshToken);
             _allegroSetttings.RefreshToken = refreshToken;
         }
 
         public void SetVerificationURIComplete(string verificationURIComplete)
         {
-            _configuration["Allegro:VerificationURIComplete"] = verificationURIComplete;
+            Save("Allegro.VerificationURIComplete", verificationURIComplete);
             _allegroSetttings.VerificationURIComplete = verificationURIComplete;
+        }
+
+       /// <summary>
+       /// Save new object in appsettings.json
+       /// </summary>
+       /// <param name="key"></param>
+       /// <param name="value"></param>
+        private void Save(string key, string value)
+        {
+            var jsonObject = GetJson();
+            var selectToken = jsonObject.SelectToken(key);
+
+            if (selectToken != null)
+            {
+                selectToken.Replace(JToken.FromObject(value));
+                File.WriteAllText($"{_appsettingsPath}", jsonObject.ToString());
+            }
+        }
+        /// <summary>
+        /// Get appsettings.json
+        /// </summary>
+        /// <returns></returns>
+        private JObject GetJson()
+        {
+            return JObject.Parse(File.ReadAllText($"{_appsettingsPath}"));
         }
     }
 }
