fc8d5325eea12215f839d73a0f295a0ae87dd6b5
Mariusz Sokołowski <mariuszsokolowski95@gmail.com>
2024-10-14 21:53:56 +0200

    Set default value for AllegroSettingsModel.

--- src/authservice/Models/AllegroSettingsModel.cs
+++ src/authservice/Models/AllegroSettingsModel.cs
@@ -2,13 +2,13 @@
 {
     public class AllegroSettingsModel
     {
-        public string BaseUrl { get; set; }
-        public string ClientId { get; set; }
-        public string ClientSecret { get; set; }
-        public string DeviceCode { get; set; }
-        public string AccessToken { get; set; }
+        public string BaseUrl { get; set; } = string.Empty;
+        public string ClientId { get; set; } = string.Empty;
+        public string ClientSecret { get; set; } = string.Empty;
+        public string DeviceCode { get; set; } = string.Empty;
+        public string AccessToken { get; set; } = string.Empty;
         public DateTime? AccessTokenExpiredDate { get; set; }
-        public string RefreshToken { get; set; }
-        public string VerificationURIComplete { get; set; }
+        public string RefreshToken { get; set; } = string.Empty;
+        public string VerificationURIComplete { get; set; } = string.Empty ;
     }
 }
