ec335edcc1d8279c2e19afb7eaa14144b3b7bddf
Mariusz Sokołowski <mariuszsokolowski95@gmail.com>
2024-10-15 20:33:24 +0200

    Add OrderRangeDTO Model

---
+++ src/orderservice/Models/OrderRangeDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace orderservice.Models
+{
+    public class OrderRangeDTO
+    {
+        [DefaultValue("2024-10-15")]
+        public string FromDate { get; set; } = string.Empty;
+        [DefaultValue("2024-10-20")]
+        public string ToDate { get; set; } = string.Empty;
+        [DefaultValue("5")]
+        public int PageSize { get; set; }
+        [DefaultValue("1")]
+        public int PageNumber { get; set; }
+    }
+}
