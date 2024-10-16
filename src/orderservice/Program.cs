568bac41256601429a87e005277e2e12af3b6be9
Mariusz Sokołowski <mariuszsokolowski95@gmail.com>
2024-10-16 19:45:51 +0200

    Add endpoint to get Orders

--- src/orderservice/Program.cs
+++ src/orderservice/Program.cs
@@ -1,9 +1,15 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using orderservice.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHttpClient();
 
 var app = builder.Build();
 
@@ -16,29 +22,48 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
+
+app.MapPost("/get", async (IHttpClientFactory httpClientFactory, IConfiguration config, [FromBody] OrderRangeDTO request) =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    var client = httpClientFactory.CreateClient();
+    var authServiceUrl = config["AuthService:BaseUrl"];
+
+    var authResponse = await client.GetAsync($"{authServiceUrl}auth");
+
+    if (!authResponse.IsSuccessStatusCode)
+    {
+        return Results.Problem("Failed to get access token from AuthService.");
+    }
+
+    var accessToken = await authResponse.Content.ReadAsStringAsync();
+
+    client.DefaultRequestHeaders.Add("Authorization", $"Bearer {accessToken}");
+    client.DefaultRequestHeaders.Add("Accept", "application/vnd.allegro.public.v1+json");
+
+    string fromDate = Convert.ToDateTime(request.FromDate).ToString("yyyy-MM-ddTHH:mm:ssZ");
+    string toDate = Convert.ToDateTime(request.ToDate).ToString("yyyy-MM-ddTHH:mm:ssZ");
+
+    var response = await client.GetAsync($"https://api.allegro.pl.allegrosandbox.pl/order/checkout-forms?lineItems.boughtAt.gte={fromDate}&lineItems.boughtAt.lte={toDate}");
+
+    if (!response.IsSuccessStatusCode)
+    {
+        return Results.Problem("Failed to fetch checkout forms from Allegro API.");
+    }
+
+    var contents = await response.Content.ReadAsStringAsync();
+    var json = JObject.Parse(contents);
+    var checkoutForms = json["checkoutForms"];
+
+    if (checkoutForms.Count() < 1)
+        return Results.NoContent();
+
+    return Results.Ok(checkoutForms);
+
 })
-.WithName("GetWeatherForecast")
+.WithName("Get")
+.WithDescription("Get orders from allegro API")
 .WithOpenApi();
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
