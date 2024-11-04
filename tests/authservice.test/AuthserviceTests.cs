f3f546e716f89e5696af3623915f96eaa3baa742
Mariusz Sokołowski <mariuszsokolowski95@gmail.com>
2024-11-04 18:11:51 +0100

    test: add program.cs endpoint tests

---
+++ tests/authservice.test/AuthserviceTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace authservice.test
+{
+    public class authserviceTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        
+        [Fact]
+        public async Task verification_endpoint_should_returns_authorization_message()
+        {
+
+            // Act
+            var response = await _client.GetAsync("/verification");
+            var responseBody = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            Assert.True(response.IsSuccessStatusCode);
+            Assert.Contains("Go to page and authorize device code", responseBody);
+        }
+
+        #region Arrange
+        private readonly HttpClient _client;
+
+        public authserviceTests(WebApplicationFactory<Program> factory)
+        {
+            _client=factory.CreateClient();
+        }
+        #endregion
+    }
+}
