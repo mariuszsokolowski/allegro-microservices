6a342a7ba5f81b8170541e651c75c84d6157bb2a
Mariusz Sokołowski <mariuszsokolowski95@gmail.com>
2024-11-09 17:58:06 +0100

    chore: add custom exption for failed refresh token request

---
+++ src/authservice/Exceptions/CustomException.cs
@@ -0,0 +1,9 @@
+﻿namespace authservice.Exceptions
+{
+    public abstract class CustomException : Exception
+    {
+        protected CustomException(string message) : base(message)
+        {
+        }
+    }
+}
---
+++ src/authservice/Exceptions/FailedRefreshTokenException.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualBasic;
+using System.Net;
+
+namespace authservice.Exceptions
+{
+    public sealed class FailedRefreshTokenException : CustomException
+    {
+        public FailedRefreshTokenException(HttpStatusCode statusCode,string contents) : base($"Request failed to get AccessToken by RefreshToken with status code {statusCode}. Response: {contents}")
+        {
+        }
+    }
+}
